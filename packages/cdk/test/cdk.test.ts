4400eac09abb3cfed77cbade95a2b8e2cca85155
SatoshiMoriyama <satoshi.moriyama@serverworks.co.jp>
2026-01-10 09:56:37 +0900

    test: スナップショットテストを追加

--- packages/cdk/test/cdk.test.ts
+++ packages/cdk/test/cdk.test.ts
@@ -1,16 +1,13 @@
-// import * as cdk from 'aws-cdk-lib/core';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as Cdk from '../lib/cdk-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import { CdkStack } from '../lib/cdk-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/cdk-stack.ts
-test('SQS Queue Created', () => {
-  //   const app = new cdk.App();
-  //     // WHEN
-  //   const stack = new Cdk.CdkStack(app, 'MyTestStack');
-  //     // THEN
-  //   const template = Template.fromStack(stack);
-  //   template.hasResourceProperties('AWS::SQS::Queue', {
-  //     VisibilityTimeout: 300
-  //   });
+describe('CdkStack', () => {
+  test('スナップショットテスト', () => {
+    const app = new cdk.App();
+    const stack = new CdkStack(app, 'TestStack');
+    const template = Template.fromStack(stack);
+
+    expect(template.toJSON()).toMatchSnapshot();
+  });
 });
